24e5faf638bc8bc303e3ddb019c3698a01168d26
Izz Abdallah <x3zoabdullah@gmail.com>
2026-03-24 15:14:02 +0400

    Setup backend with Express and Prisma

---
+++ backend/dist/app.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from 'express';
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Test this route");
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost: ${PORT}`);
+});
+//# sourceMappingURL=app.js.map
